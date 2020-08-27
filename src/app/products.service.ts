50f5382bf7d8bac315708608a396df65e6d7fff5
chanaka lakshan <chanakabalasuriya123@gmail.com>
2020-08-27 16:00:51 +0530

    19.1.3th commit( trying to deploye in heroku)

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,148 +1,184 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+const BACKEND_URL = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductsService {
+  private url = BACKEND_URL;
 
-  url:string = "http://localhost:3000/api/"
+  private _getItemsUrl = this.url + "getItems";
+  private _getitemsbyidsUrl = this.url + "getitemsbyids";
+  private _getProductDetailsHistoryUrl = this.url + "getProductDetailsHistory";
+  private _getProductDetailsUrl = this.url + "getProductDetails";
+  private _getMainCategoriesListUrl = this.url + "getMainCategoriesList";
+  private _getMainCategoriesListAdminUrl =
+    this.url + "getMainCategoriesListAdmin";
+  private _getMainCategoryProductsUrl = this.url + "getMainCategoryProducts";
+  private _mainSearchProductsUrl = this.url + "mainSearchProducts";
+  private _getAllProductsUrl = this.url + "getAllProducts";
+  private _addProductUrl = this.url + "addProduct";
+  private _updateProductImgUrl = this.url + "updateProductImg";
+  private _updateProdcutsDetailsUrl = this.url + "updateProdcutsDetails";
+  private _updateProductTagsUrl = this.url + "updateProductTags";
+  private _updateMainCategoryVisibiltyUrl =
+    this.url + "updateMainCategoryVisibilty";
+  private _removeCategoryFromHomeUrl = this.url + "removeCategoryFromHome";
+  private _removeProductFromHomeUrl = this.url + "removeProductFromHome";
+  private _addMainCategoriesTitleUrl = this.url + "addMainCategoriesTitle";
+  private _addMainCategoryProductsUrl = this.url + "addMainCategoryProducts";
+  private _uploadProductImgUrl = this.url + "profile";
+  private _productsOrderUrl = this.url + "productsOrder";
+  private _getOrderListUrl = this.url + "getOrderList";
+  private _getOrderByTrackIdUrl = this.url + "getOrderByTrackId";
+  private _getOrderByUserIdUrl = this.url + "getOrderByUserId";
+  private _getOrderDetailsByTrackIdUrl = this.url + "getOrderDetailsByTrackId";
+  private _searchOrderReqTableUrl = this.url + "searchorderreqtable";
+  private _ordersStatusChangeUrl = this.url + "orderStatusChange";
+  private _updateProductVisibiltyUrl = this.url + "updateProductVisibilty";
+  private _updateProductDiscountOnOffUrl =
+    this.url + "updateProductDiscountOnOff";
 
-  private _getItemsUrl = this.url+"getItems";
-  private _getitemsbyidsUrl = this.url+"getitemsbyids";
-  private _getProductDetailsHistoryUrl = this.url+"getProductDetailsHistory";
-  private _getProductDetailsUrl = this.url+"getProductDetails";
-  private _getMainCategoriesListUrl = this.url+"getMainCategoriesList";
-  private _getMainCategoriesListAdminUrl = this.url+"getMainCategoriesListAdmin";
-  private _getMainCategoryProductsUrl = this.url+"getMainCategoryProducts";
-  private _mainSearchProductsUrl = this.url+"mainSearchProducts";
-  private _getAllProductsUrl = this.url+"getAllProducts";
-  private _addProductUrl = this.url+"addProduct";
-  private _updateProductImgUrl = this.url+"updateProductImg";
-  private _updateProdcutsDetailsUrl = this.url+"updateProdcutsDetails";
-  private _updateProductTagsUrl = this.url+"updateProductTags";
-  private _updateMainCategoryVisibiltyUrl = this.url+"updateMainCategoryVisibilty";
-  private _removeCategoryFromHomeUrl = this.url+"removeCategoryFromHome";
-  private _removeProductFromHomeUrl = this.url+"removeProductFromHome";
-  private _addMainCategoriesTitleUrl = this.url+"addMainCategoriesTitle";
-  private _addMainCategoryProductsUrl = this.url+"addMainCategoryProducts";
-  private _uploadProductImgUrl = this.url+"profile"
-  private _productsOrderUrl = this.url+"productsOrder"
-  private _getOrderListUrl = this.url+"getOrderList"
-  private _getOrderByTrackIdUrl = this.url+"getOrderByTrackId"
-  private _getOrderByUserIdUrl = this.url+"getOrderByUserId"
-  private _getOrderDetailsByTrackIdUrl = this.url+"getOrderDetailsByTrackId"
-  private _searchOrderReqTableUrl = this.url+"searchorderreqtable"
-  private _ordersStatusChangeUrl = this.url+"orderStatusChange";
-  private _updateProductVisibiltyUrl = this.url+"updateProductVisibilty"
-  private _updateProductDiscountOnOffUrl = this.url+"updateProductDiscountOnOff"
+  constructor(private http: HttpClient) {}
 
-  constructor( private http: HttpClient) { }
-
-  getMainCategoriesList(){
-    return this.http.get<any> (this._getMainCategoriesListUrl)
+  getMainCategoriesList() {
+    return this.http.get<any>(this._getMainCategoriesListUrl);
   }
-  getMainCategoriesListAdmin(){
-    return this.http.get<any> (this._getMainCategoriesListAdminUrl)
+  getMainCategoriesListAdmin() {
+    return this.http.get<any>(this._getMainCategoriesListAdminUrl);
   }
-  getMainCategoryProducts(category){
-    return this.http.get<any> (this._getMainCategoryProductsUrl,{ params : { category : category }})
+  getMainCategoryProducts(category) {
+    return this.http.get<any>(this._getMainCategoryProductsUrl, {
+      params: { category: category },
+    });
   }
-  mainSearchProducts(details){
+  mainSearchProducts(details) {
     //return this.http.get<any>(this._mainSearchProductsUrl,{ params : details })
-    return this.http.get<any>(this._mainSearchProductsUrl,{ params : { details : details } })
+    return this.http.get<any>(this._mainSearchProductsUrl, {
+      params: { details: details },
+    });
   }
-  getAllProducts(){
-    return this.http.get<any> ( this._getAllProductsUrl)
+  getAllProducts() {
+    return this.http.get<any>(this._getAllProductsUrl);
   }
-  getItems(shop){
-    return this.http.get<any> (this._getItemsUrl,{ params : { category : shop }})
+  getItems(shop) {
+    return this.http.get<any>(this._getItemsUrl, {
+      params: { category: shop },
+    });
   }
-  getitemsbyids(ids){
-    return this.http.get<any> (this._getitemsbyidsUrl,{ params : { idList : ids }})
+  getitemsbyids(ids) {
+    return this.http.get<any>(this._getitemsbyidsUrl, {
+      params: { idList: ids },
+    });
   }
-  getProductDetails(id){
-    return this.http.get<any> (this._getProductDetailsUrl,{ params : { productId : id }})
+  getProductDetails(id) {
+    return this.http.get<any>(this._getProductDetailsUrl, {
+      params: { productId: id },
+    });
   }
-  getProductDetailsHistory(id){
-    return this.http.get<any> (this._getProductDetailsHistoryUrl,{ params : { productId : id }})
+  getProductDetailsHistory(id) {
+    return this.http.get<any>(this._getProductDetailsHistoryUrl, {
+      params: { productId: id },
+    });
   }
-  addProduct(details){
-    return this.http.post<any>( this._addProductUrl,details , {
-      reportProgress : true,
-      observe: 'events'
-    })
+  addProduct(details) {
+    return this.http.post<any>(this._addProductUrl, details, {
+      reportProgress: true,
+      observe: "events",
+    });
   }
-  updateProductImg(details){
-    return this.http.post<any>( this._updateProductImgUrl,details , {
-      reportProgress : true,
-      observe: 'events'
-    })
+  updateProductImg(details) {
+    return this.http.post<any>(this._updateProductImgUrl, details, {
+      reportProgress: true,
+      observe: "events",
+    });
   }
-  updateProdcutsDetails(details){
-    return this.http.post<any> (this._updateProdcutsDetailsUrl,details)
+  updateProdcutsDetails(details) {
+    return this.http.post<any>(this._updateProdcutsDetailsUrl, details);
   }
-  updateProductTags(tags,id){
-    return this.http.post<any> (this._updateProductTagsUrl,{ params : { tags : tags , id : id } })
+  updateProductTags(tags, id) {
+    return this.http.post<any>(this._updateProductTagsUrl, {
+      params: { tags: tags, id: id },
+    });
     //return this.http.get<any>(this._mainSearchProductsUrl,{ params : details })
     //return this.http.get<any>(this._mainSearchProductsUrl,{ params : { tags : tags } })
   }
-  changeProductVisibilty(id){
-    return this.http.post<any>(this._updateProductVisibiltyUrl, { params : { productId : id }})
+  changeProductVisibilty(id) {
+    return this.http.post<any>(this._updateProductVisibiltyUrl, {
+      params: { productId: id },
+    });
   }
-  changeProductDiscountOnOff(id){
-    return this.http.post<any>(this._updateProductDiscountOnOffUrl, { params : { productId : id }})
+  changeProductDiscountOnOff(id) {
+    return this.http.post<any>(this._updateProductDiscountOnOffUrl, {
+      params: { productId: id },
+    });
   }
-  addMainCategoriesTitle(product){
-    return this.http.post<any>(this._addMainCategoriesTitleUrl,product);
+  addMainCategoriesTitle(product) {
+    return this.http.post<any>(this._addMainCategoriesTitleUrl, product);
   }
-  addMainCategoryProducts(product){
-    return this.http.post<any>(this._addMainCategoryProductsUrl,product);
-  }
-
-  changeStatus(newStatus,id){
-    return this.http.post<any>(this._ordersStatusChangeUrl,{params : { sid : id  , status : newStatus } });
-  }
-
-  removeCategoryFromHome(category){
-    return this.http.post<any>(this._removeCategoryFromHomeUrl, { params : { category : category }} )
-  }
-  changeMainCategoryVisibilty(category){
-    return this.http.post<any>(this._updateMainCategoryVisibiltyUrl, { params : { category : category }})
-  }
-  removeProductFromHome(id){
-    return this.http.post<any>(this._removeProductFromHomeUrl, { params : { item : id }})
-  }
-  uploadProductImg(img){
-    return this.http.post<any>( this._uploadProductImgUrl, img , {
-      reportProgress : true,
-      observe: 'events'
-    })
+  addMainCategoryProducts(product) {
+    return this.http.post<any>(this._addMainCategoryProductsUrl, product);
   }
 
-
-  productsOrder(details){
-    return this.http.post<any> ( this._productsOrderUrl , { params : { orders : details }});
+  changeStatus(newStatus, id) {
+    return this.http.post<any>(this._ordersStatusChangeUrl, {
+      params: { sid: id, status: newStatus },
+    });
   }
 
-
-  getOrderList(){
-    return this.http.get<any> ( this._getOrderListUrl);
+  removeCategoryFromHome(category) {
+    return this.http.post<any>(this._removeCategoryFromHomeUrl, {
+      params: { category: category },
+    });
   }
-  getOrderByTrackId(id){
-    return this.http.get<any>( this._getOrderByTrackIdUrl , { params : { trackId : id }})
+  changeMainCategoryVisibilty(category) {
+    return this.http.post<any>(this._updateMainCategoryVisibiltyUrl, {
+      params: { category: category },
+    });
   }
-  getOrderByUserId(id){
-    return this.http.get<any>( this._getOrderByUserIdUrl , { params : { UserId : id }})
+  removeProductFromHome(id) {
+    return this.http.post<any>(this._removeProductFromHomeUrl, {
+      params: { item: id },
+    });
   }
-  getOrderDetailsByTrackId(id){
-    console.log(id)
-    return this.http.get<any>( this._getOrderDetailsByTrackIdUrl , { params : { trackId : id }})
+  uploadProductImg(img) {
+    return this.http.post<any>(this._uploadProductImgUrl, img, {
+      reportProgress: true,
+      observe: "events",
+    });
   }
 
+  productsOrder(details) {
+    return this.http.post<any>(this._productsOrderUrl, {
+      params: { orders: details },
+    });
+  }
 
+  getOrderList() {
+    return this.http.get<any>(this._getOrderListUrl);
+  }
+  getOrderByTrackId(id) {
+    return this.http.get<any>(this._getOrderByTrackIdUrl, {
+      params: { trackId: id },
+    });
+  }
+  getOrderByUserId(id) {
+    return this.http.get<any>(this._getOrderByUserIdUrl, {
+      params: { UserId: id },
+    });
+  }
+  getOrderDetailsByTrackId(id) {
+    console.log(id);
+    return this.http.get<any>(this._getOrderDetailsByTrackIdUrl, {
+      params: { trackId: id },
+    });
+  }
 
-  searchOrderReqTable(params){ // order-request component
-    return this.http.get<any>( this._searchOrderReqTableUrl , { params : params })
+  searchOrderReqTable(params) {
+    // order-request component
+    return this.http.get<any>(this._searchOrderReqTableUrl, { params: params });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,70 +1,70 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { catchError, mapTo, tap } from 'rxjs/operators'
-import { Observable, of, BehaviorSubject} from 'rxjs';
-import { Tokens } from '../Models/Token';
-import { User } from '../Models/user';
-import decode from 'jwt-decode';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { catchError, mapTo, tap } from "rxjs/operators";
+import { Observable, of, BehaviorSubject } from "rxjs";
+import { Tokens } from "../Models/Token";
+import { User } from "../Models/user";
+import decode from "jwt-decode";
+
+import { environment } from "../../environments/environment";
+
+const BACKEND_URL = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  private url = BACKEND_URL;
 
-  url:string = "http://localhost:3000/api/"
+  private _registerUrl = this.url + "register";
+  private _loginUrl = this.url + "login";
+  private _getPermisionUserUrl = this.url + "getPermisionUser";
+  private _confirmEmailUrl = this.url + "confirmemail";
+  private _getbasicuserdetailsbyuidUrl = this.url + "getbasicuserdetailsbyuid";
+  private _updateprofileUrl = this.url + "updateprofile";
+  private _updatepassword = this.url + "updatepassword";
 
-  private _registerUrl = this.url+"register"
-  private _loginUrl = this.url+"login"
-  private _getPermisionUserUrl = this.url+"getPermisionUser"
-  private _confirmEmailUrl = this.url+"confirmemail"
-  private _getbasicuserdetailsbyuidUrl= this.url+"getbasicuserdetailsbyuid"
-  private _updateprofileUrl = this.url+"updateprofile";
-  private _updatepassword = this.url+"updatepassword"
+  user$: Observable<User>;
 
-  user$ : Observable<User>;
-
-  private loggedUser:string
-
+  private loggedUser: string;
 
   private newLogin = new BehaviorSubject(false);
   newLoginStatus = this.newLogin.asObservable();
 
+  constructor(private http: HttpClient, private _router: Router) {}
 
-  constructor(private http:HttpClient , private _router:Router ) { }
-  
-  registerUser( user ){
-      return this.http.post<any>(this._registerUrl, user)
+  registerUser(user) {
+    return this.http.post<any>(this._registerUrl, user);
   }
-  confirmAccount(details){
-    return this.http.post<any>(this._confirmEmailUrl,{ details : details } )
+  confirmAccount(details) {
+    return this.http.post<any>(this._confirmEmailUrl, { details: details });
   }
-  loginUser( user ): Observable<boolean>{
-    return this.http.post<any>(this._loginUrl, user)
-    .pipe(
-      tap(token => this.doLoginUser(user.uName, token)),
+  loginUser(user): Observable<boolean> {
+    return this.http.post<any>(this._loginUrl, user).pipe(
+      tap((token) => this.doLoginUser(user.uName, token)),
       mapTo(true),
-      catchError(error => {
+      catchError((error) => {
         alert(error.error);
         return of(false);
       })
-      );
-    }
-
-  getbasicuserdetailsbyuid(){
-    return this.http.get<any>(this._getbasicuserdetailsbyuidUrl )
-  }
-  updateprofile(details){
-    return this.http.post<any>(this._updateprofileUrl,details )
-  }
-  updatepassword(details){
-    return this.http.post<any>(this._updatepassword,details )
+    );
   }
 
-  logout(){
-    localStorage.removeItem('token')
+  getbasicuserdetailsbyuid() {
+    return this.http.get<any>(this._getbasicuserdetailsbyuidUrl);
+  }
+  updateprofile(details) {
+    return this.http.post<any>(this._updateprofileUrl, details);
+  }
+  updatepassword(details) {
+    return this.http.post<any>(this._updatepassword, details);
+  }
+
+  logout() {
+    localStorage.removeItem("token");
     this.getLoginStatus(true);
-    this._router.navigate(['/login'])  
+    this._router.navigate(["/login"]);
 
     // const data : User = {
     //   uid: user.uid,
@@ -76,54 +76,53 @@ export class AuthService {
     // }
   }
 
-  getPermisionUser(){
-    return this.http.get<any>(this._getPermisionUserUrl)
-  }
-  
-
-  loggedIn(){
-    return !!localStorage.getItem('token')
+  getPermisionUser() {
+    return this.http.get<any>(this._getPermisionUserUrl);
   }
 
-  doLoginUser(username:string, tokens: Tokens){
+  loggedIn() {
+    return !!localStorage.getItem("token");
+  }
+
+  doLoginUser(username: string, tokens: Tokens) {
     this.loggedUser = username;
-    this.storeToken(tokens)
+    this.storeToken(tokens);
   }
 
-  storeToken(token: Tokens){
-    localStorage.setItem('token', token.jwtToken)
+  storeToken(token: Tokens) {
+    localStorage.setItem("token", token.jwtToken);
   }
 
-  getToken(){
-    return localStorage.getItem('token')
+  getToken() {
+    return localStorage.getItem("token");
   }
 
-  removeToken(){
-    return localStorage.removeItem('token')
+  removeToken() {
+    return localStorage.removeItem("token");
   }
 
   decode() {
     //const gg = decode(localStorage.getItem('token'))
     //console.log(gg.subject[0].userName)
-    return decode(localStorage.getItem('token'));
+    return decode(localStorage.getItem("token"));
   }
-  checkAuthorization():boolean{
+  checkAuthorization(): boolean {
     if (this.loggedIn()) {
       const user = this.decode();
 
       if (user !== undefined || user === null) {
-        if (user.subject[0].role === 'admin') {
-          console.log('admin accessed')
+        if (user.subject[0].role === "admin") {
+          console.log("admin accessed");
           return true;
         }
       }
     }
-    
-     console.log('admin denied')
-      return false
+
+    console.log("admin denied");
+    return false;
   }
 
-  getLoginStatus( newLoginInstance: boolean){
-    this.newLogin.next(newLoginInstance)
+  getLoginStatus(newLoginInstance: boolean) {
+    this.newLogin.next(newLoginInstance);
   }
 }
